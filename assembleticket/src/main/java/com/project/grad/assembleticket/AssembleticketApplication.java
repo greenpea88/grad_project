c7697e38cea1fbdd501a322c07e9f528d85bbf4d
greenpea88 <green_pea88@naver.com>
2020-07-16 20:31:19 +0900

    new spring project

---
+++ assembleticket/src/main/java/com/project/grad/assembleticket/AssembleticketApplication.java
@@ -0,0 +1,13 @@
+package com.project.grad.assembleticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssembleticketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssembleticketApplication.class, args);
+    }
+
+}
