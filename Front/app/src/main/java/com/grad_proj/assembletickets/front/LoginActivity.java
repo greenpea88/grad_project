4a1994fb6f128d009c00c6696decbfe10e62996f
greenpea88 <green_pea88@naver.com>
2020-09-04 00:31:37 +0900

    login 화면 추가

---
+++ Front/app/src/main/java/com/grad_proj/assembletickets/front/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.grad_proj.assembletickets.front;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
