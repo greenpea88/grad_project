da30b655718fc514b5824016dbff3969169e8eb0
greenpea88 <green_pea88@naver.com>
2020-09-22 21:24:07 +0900

    calendar today 표시

---
+++ Front/app/src/main/java/com/grad_proj/assembletickets/front/CalendarDeco/OneDayDeco.java
@@ -0,0 +1,43 @@
+package com.grad_proj.assembletickets.front.CalendarDeco;
+
+import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
+import android.view.ViewGroup;
+
+import androidx.core.content.res.ResourcesCompat;
+
+import com.grad_proj.assembletickets.front.R;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+
+public class OneDayDeco implements DayViewDecorator {
+    //오늘에 해당하는 날짜에 표시하기
+
+    private CalendarDay date;
+    private final Drawable drawable;
+
+    public OneDayDeco(ViewGroup context){
+        date = CalendarDay.today();
+        drawable = ResourcesCompat.getDrawable(context.getResources(),R.drawable.today_calendar,null);
+    }
+
+    @Override
+    public boolean shouldDecorate(CalendarDay day) {
+        return date != null && day.equals(date);
+    }
+
+    @Override
+    public void decorate(DayViewFacade view) {
+        view.setBackgroundDrawable(drawable);
+        view.addSpan(new StyleSpan(Typeface.BOLD));
+        view.addSpan(new RelativeSizeSpan(1.4f));
+        view.addSpan(new ForegroundColorSpan(Color.WHITE));
+    }
+}
--- Front/app/src/main/java/com/grad_proj/assembletickets/front/Fragment/CalendarFragment.java
+++ Front/app/src/main/java/com/grad_proj/assembletickets/front/Fragment/CalendarFragment.java
@@ -1,5 +1,6 @@
 package com.grad_proj.assembletickets.front.Fragment;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,13 +9,23 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.grad_proj.assembletickets.front.CalendarDeco.OneDayDeco;
 import com.grad_proj.assembletickets.front.R;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
 public class CalendarFragment extends Fragment {
+
+    MaterialCalendarView materialCalendarView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        materialCalendarView = rootView.findViewById(R.id.calendarView);
+        materialCalendarView.addDecorator(new OneDayDeco(rootView));
+        return rootView;
     }
 }
