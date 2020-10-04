85fa80d3154326b7ac278e8eaeec4ce230e51d15
YYJ <hidua1124@naver.com>
2020-10-05 00:59:07 +0900

    수정: ShowRepository 쿼리 메서드 정리
    - 기본적으로 제공되는 메서드(findAll, findById) 삭제
    - 엔티티의 칼럼명과 일치하지 않아 쿼리가 실행되지 않는 이슈 해결 (findByCategory → findByType)

--- Server/src/main/java/com/project/grad/assembleticket/domain/repository/ShowRepository.java
+++ Server/src/main/java/com/project/grad/assembleticket/domain/repository/ShowRepository.java
@@ -5,12 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface ShowRepository extends JpaRepository<Shows, Long> {
-    Optional<Shows> findById(Long id);
-    List<Shows> findByTitleContaining(String title);
-    List<Shows> findByCategory(int type);
-    List<Shows> findAll();
+
+    List<Shows> findAllByTitleContaining(String title);
+    List<Shows> findAllByType(int type);
+
 }
