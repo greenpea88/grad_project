f032ac9cbe43d1b24fe908c5de81b6dd3b872a2b
greenpea88 <green_pea88@naver.com>
2020-11-14 23:31:38 +0900

    변경: event decorator 모양 변경

--- Front/app/src/main/java/com/grad_proj/assembletickets/front/CalendarDeco/EventDeco.java
+++ Front/app/src/main/java/com/grad_proj/assembletickets/front/CalendarDeco/EventDeco.java
@@ -8,18 +8,21 @@ import com.grad_proj.assembletickets.front.R;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.DayViewDecorator;
 import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.spans.DotSpan;
 
 import java.util.Collection;
 import java.util.HashSet;
 
 public class EventDeco implements DayViewDecorator {
 
-    private final Drawable drawable;
+//    private final Drawable drawable;
     private HashSet<CalendarDay> days;
+    private Context context;
 
     public EventDeco(Collection<CalendarDay> days, Context context){
-        drawable = context.getResources().getDrawable(R.drawable.event_deco);
+//        drawable = context.getResources().getDrawable(R.drawable.event_deco);
         this.days = new HashSet<>(days);
+        this.context=context;
     }
 
     @Override
@@ -29,6 +32,6 @@ public class EventDeco implements DayViewDecorator {
 
     @Override
     public void decorate(DayViewFacade view) {
-        view.setSelectionDrawable(drawable);
+        view.addSpan(new DotSpan(8,context.getColor(R.color.colorFastWithUs1)));
     }
 }
